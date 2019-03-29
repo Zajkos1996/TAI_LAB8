f8fc27bdf9eab061db67f543a71000e68068d1b5
Zajkos1996 <zajkos016@gmail.com>
2019-03-29 16:43:40 +0100

    Dodanie nawigacji :)

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { QuizComponent } from './components/quiz/quiz.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { ContactComponent } from './components/contact/contact.component';
     HomeComponent,
     BlogComponent,
     QuizComponent,
-    ContactComponent
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
