7561d2854d5d5e47188b19861ab2fc56142f68d2
Zajkos1996 <zajkos016@gmail.com>
2019-03-29 16:00:30 +0100

    TypeScript

---
+++ complex.ts
@@ -0,0 +1,32 @@
+class Complex {
+    private rzeczywista: number;
+    private urojona: number;
+
+    constructor(rzeczywsita: number, urojona: number) {
+        this.rzeczywista = rzeczywsita;
+        this.urojona = urojona;
+    }
+
+    Add(obj: Complex): Complex {
+        return new Complex(this.rzeczywista + obj.rzeczywista , this.urojona + obj.urojona );
+    }
+
+    Substract(obj: Complex): Complex {
+        return new Complex(this.rzeczywista - obj.rzeczywista , this.urojona - obj.urojona );
+    }
+
+    Modul (){
+       return Math.sqrt((this.rzeczywista * this.rzeczywista) + (this.urojona*this.urojona));
+    }
+    toString(){
+        return this.rzeczywista + "+" + this.urojona + "i";
+    }
+}
+let obj = new Complex(3,2);
+let obj1 = new Complex(2,1);
+let obj2 = obj.Add(obj1);
+let obj3 = obj.Substract(obj1);
+let obj4 = obj.Modul();
+console.log("Add = " + obj2);
+console.log("Substract = " + obj3);
+console.log("Modul = " + obj4);
