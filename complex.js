cf612675b019084b45c79ee287b32319cb94eee3
Zajkos1996 <zajkos016@gmail.com>
2019-03-29 16:03:29 +0100

    Transpilacja TypeScript

--- complex.js
+++ complex.js
@@ -1,27 +1,27 @@
-var Complex = /** @class */ (function () {
-    function Complex(rzeczywsita, urojona) {
+"use strict";
+class Complex {
+    constructor(rzeczywsita, urojona) {
         this.rzeczywista = rzeczywsita;
         this.urojona = urojona;
     }
-    Complex.prototype.Add = function (obj) {
+    Add(obj) {
         return new Complex(this.rzeczywista + obj.rzeczywista, this.urojona + obj.urojona);
-    };
-    Complex.prototype.Substract = function (obj) {
+    }
+    Substract(obj) {
         return new Complex(this.rzeczywista - obj.rzeczywista, this.urojona - obj.urojona);
-    };
-    Complex.prototype.Modul = function () {
+    }
+    Modul() {
         return Math.sqrt((this.rzeczywista * this.rzeczywista) + (this.urojona * this.urojona));
-    };
-    Complex.prototype.toString = function () {
+    }
+    toString() {
         return this.rzeczywista + "+" + this.urojona + "i";
-    };
-    return Complex;
-}());
-var obj = new Complex(3, 2);
-var obj1 = new Complex(2, 1);
-var obj2 = obj.Add(obj1);
-var obj3 = obj.Substract(obj1);
-var obj4 = obj.Modul();
+    }
+}
+let obj = new Complex(3, 2);
+let obj1 = new Complex(2, 1);
+let obj2 = obj.Add(obj1);
+let obj3 = obj.Substract(obj1);
+let obj4 = obj.Modul();
 console.log("Add = " + obj2);
 console.log("Substract = " + obj3);
 console.log("Modul = " + obj4);
